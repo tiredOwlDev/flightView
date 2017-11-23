510e9a078a547c390e7e78561e9e60a29a24d6c1
tiredOwl <tiredOwlDev@gmail.com>
2017-11-23 17:42:47 -0600

    Minor updates and testing
    Added error catch to dispatch timer, added invisible button to canvas 0 for testing

--- flightView/MainWindow.xaml.cs
+++ flightView/MainWindow.xaml.cs
@@ -46,44 +46,49 @@ namespace flightView
 
             while ((i < RawGameController.RawGameControllers.Count) && (i < 3)) 
             {
-                // Grab the first controller
-                _controller = RawGameController.RawGameControllers.ElementAt(i);
-
-                // Register XAML names
-                System.Windows.Shapes.Ellipse _joystickPos = (System.Windows.Shapes.Ellipse)FindName("JoystickPos" + i);
-                System.Windows.Shapes.Rectangle _joystickVec = (System.Windows.Shapes.Rectangle)FindName("JoystickVec" + i);
-
-                // fieldTB = (TextBox)this.FindName("Field_CompanyName");
-
-                // Initialize array of readings
-                var _buttonReadings = new bool[_controller.ButtonCount];
-                var _switchReadings = new GameControllerSwitchPosition[_controller.SwitchCount];
-                var _axisReadings = new double[_controller.AxisCount];
-
-                // Get readings from controller
-                _controller.GetCurrentReading(_buttonReadings, _switchReadings, _axisReadings);
-
-                // Find transformations
-                double _verticalAxisMargin = _axisReadings[1] * 200 + 25;
-                double _horizontalAxisMargin = _axisReadings[0] * 200 + 25;
-                System.Windows.Media.RotateTransform rotateTransform = new System.Windows.Media.RotateTransform();
-                // Check if the axis is related to the pedals and then reverse
-                if (i != 2)
+                try
                 {
-                    rotateTransform.Angle = _axisReadings[2] * 90 - 45;
-                }
-                else
-                {
-                    rotateTransform.Angle = (_axisReadings[2] * 90 - 45) * (-1);
-                }
+                    // Grab the first controller
+                    _controller = RawGameController.RawGameControllers.ElementAt(i);
 
-                // Apply transformation
-                _joystickPos.Margin = new System.Windows.Thickness(_horizontalAxisMargin, _verticalAxisMargin, 0, 0);
-                if (i != 2)
-                {
-                    _joystickVec.Margin = new System.Windows.Thickness(_horizontalAxisMargin + 20, _verticalAxisMargin, 0, 0);
+                    // Register XAML names
+                    System.Windows.Shapes.Ellipse _joystickPos = (System.Windows.Shapes.Ellipse)FindName("JoystickPos" + i);
+                    System.Windows.Shapes.Rectangle _joystickVec = (System.Windows.Shapes.Rectangle)FindName("JoystickVec" + i);
+
+                    // Initialize array of readings
+                    var _buttonReadings = new bool[_controller.ButtonCount];
+                    var _switchReadings = new GameControllerSwitchPosition[_controller.SwitchCount];
+                    var _axisReadings = new double[_controller.AxisCount];
+
+                    // Get readings from controller
+                    _controller.GetCurrentReading(_buttonReadings, _switchReadings, _axisReadings);
+
+                    // Find transformations
+                    double _verticalAxisMargin = _axisReadings[1] * 200 + 25;
+                    double _horizontalAxisMargin = _axisReadings[0] * 200 + 25;
+                    System.Windows.Media.RotateTransform rotateTransform = new System.Windows.Media.RotateTransform();
+                    // Check if the axis is related to the pedals and then reverse
+                    if (i != 2)
+                    {
+                        rotateTransform.Angle = _axisReadings[2] * 90 - 45;
+                    }
+                    else
+                    {
+                        rotateTransform.Angle = (_axisReadings[2] * 90 - 45) * (-1);
+                    }
+
+                    // Apply transformation
+                    _joystickPos.Margin = new System.Windows.Thickness(_horizontalAxisMargin, _verticalAxisMargin, 0, 0);
+                    if (i != 2)
+                    {
+                        _joystickVec.Margin = new System.Windows.Thickness(_horizontalAxisMargin + 20, _verticalAxisMargin, 0, 0);
+                    }
+                    _joystickVec.RenderTransform = rotateTransform;
                 }
-                _joystickVec.RenderTransform = rotateTransform;
+                catch
+                {
+                    // Add error logging here
+                }
 
                 // Increase loop control
                 i++;
@@ -94,5 +99,8 @@ namespace flightView
         {
             Keyboard.ClearFocus();
         }
+
+
+
     }
 }
